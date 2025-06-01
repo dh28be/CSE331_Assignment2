5d5a20928ef85c7f75674f26b1e4cacdd186d71c
dh28be <dh28be@gmail.com>
2025-06-02 03:25:07 +0900

    first commit

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define N 200005
+#define x first
+#define y second
+
+using namespace std;
+
+typedef long long ll;
+typedef pair<ll, ll> pll;
+
+int main() {
+    ios_base :: sync_with_stdio(false); 
+    cin.tie(NULL);
+    ll t, n, ans;
+    cin >> t;
+    while(t--) {
+        cin >> n;
+        cout << ans << '\n';
+    }
+    return 0;
+}
